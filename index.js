ba38c2f5a9342139108fc6d5bc9590c2fb0745e0
André Luiz França Batista <andre.iftm@gmail.com>
2023-10-17 10:19:26 -0300

    create model

--- index.js
+++ index.js
@@ -16,32 +16,6 @@ database.once('connected', () => {
     console.log('Database Connected');
 })
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = mongoString;
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
 const app = express();
 
 app.use(express.json());
